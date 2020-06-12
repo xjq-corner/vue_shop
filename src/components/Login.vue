08b0c1c7849698400778d2d36f70ae7693a0876b
xjq_cornenr <412357351@qq.com>
2020-06-12 21:42:50 +0800

    em

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -41,8 +41,8 @@ export default {
     return {
       //这是登录表单的数据绑定对象
       loginForm: {
-        username: "admin",
-        password: "123456"
+        username: "",
+        password: ""
       },
       //这是表单的验证规则
       loginFormRules: {
