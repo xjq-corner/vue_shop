ac30df827ea349dc51d3eac0676ea4d3e11eda99
412357351@qq.com <412357351@qq.com>
2020-07-04 12:30:08 +0800

    not found

---
+++ src/components/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Not Found</p>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+  p{
+    font-size: 20px;
+    font-weight: 800;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import NotFound from '../components/NotFound.vue'
 
 
 Vue.use(VueRouter)
@@ -9,7 +10,8 @@ Vue.use(VueRouter)
 const routes = [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
-    { path: '/home', component: Home }
+    { path: '/home', component: Home },
+    { path: '*', component: NotFound }
 ]
 
 const router = new VueRouter({
@@ -19,16 +21,16 @@ const router = new VueRouter({
 })
 
 //挂载路由导航守卫
-router.beforeEach((to, from, next) => {
-    //to 将要访问的路径
-    //from 代表从哪个路径跳转而来
-    //next 是一个函数，表示放行
-    //    next()  放行   next('/login') 强制跳转
+// router.beforeEach((to, from, next) => {
+//     //to 将要访问的路径
+//     //from 代表从哪个路径跳转而来
+//     //next 是一个函数，表示放行
+//     //    next()  放行   next('/login') 强制跳转
 
-    if (to.path === '/login') return next()
-        //获取token
-    const tokenStr = window.sessionStorage.getItem('token')
-    if (!tokenStr) return next('/login')
-    next()
-})
+//     if (to.path === '/login') return next()
+//         //获取token
+//     const tokenStr = window.sessionStorage.getItem('token')
+//     if (!tokenStr) return next('/login')
+//     next()
+// })
 export default router
